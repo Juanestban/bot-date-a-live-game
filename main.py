5276e3e685331ccb6bb59ebdefcf104b97d18081
Juanestban <juanestban000@gmail.com>
2021-11-20 14:03:50 -0500

    udating

--- main.py
+++ main.py
@@ -23,12 +23,50 @@ def runCommad(cmd):
 
 
 ### coords [x, y]
-coords = ["86", "240"]
+iconHome = ["1536", "50"]
+iconBack = ["50", "50"]
+iconSendTask = ["1400", "800"]
+ButtonClainAll = ["1550", "180"]
+# tip => do it steps manual one time and write steps for implement with coords [x,y]
+coordsNew = [
+    "86", "240", "850", "850", "850", "850", *iconHome, "1090", "360", "1470",
+    "160", "1470", "160", *iconHome, "1200", "330", "1200", "840", "1400",
+    "840", *iconHome, "1520", "830", "270", "580", "1200", "680", "1200",
+    "680", "1300", "800", "1080", "612", *iconBack, "1520", "360", *iconHome,
+    "140", "270", *iconSendTask, "60", "150", *iconSendTask, "60", "280",
+    *iconSendTask, "60", "440", *iconSendTask, "60", "590", *iconSendTask,
+    "60", "760", *iconSendTask, *iconHome
+]
+coordsWithout = [
+    "86", "240", "850", "850", "850", "850", *iconHome, "1090", "360", "1470",
+    "160", "1470", "160", *iconHome, "1200", "330", "1200", "840", "1400",
+    "840", *iconHome, "1530", "830", "270", "580", "1200", "680", "1200",
+    "680", "1300", "800", "1080", "612", *iconBack, "1520", "360", "140",
+    "270", *iconSendTask, "60", "150", *iconSendTask, "60", "280",
+    *iconSendTask, "60", "440", *iconSendTask, "60", "590", *iconSendTask,
+    "60", "760", *iconSendTask, *iconHome
+]
+# look part when iconBack & iconHome are used so close
 
 ### Client ADB Device
 client = Client(host="127.0.0.1", port=5037)
 devices = client.devices()
 device = devices[0]
-
+print(len(coordsNew) / 2, len(coordsWithout) / 2)
 ### Exce command
-device.shell("input touchscreen tap {x} {y}".format(x=coords[0], y=coords[1]))
+# device.shell("input touchscreen tap {x} {y}".format(x=coords[4], y=coords[5]))
+
+count = 0
+step = 1
+stepsComplete = len(coordsWithout) / 2
+
+while stepsComplete >= step:
+    count += 2
+    coords = coordsWithout
+    x = coords[count - 2]
+    y = coords[count - 1]
+
+    device.shell("input touchscreen tap {x} {y}".format(x=x, y=y))
+    print(x, y)
+    input('are you wish continue?\n')
+    step += 1
