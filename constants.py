490ae600e92c3f8c6af4f4fd3683b2ea92857d17
Juanestban <juanestban000@gmail.com>
2022-03-05 18:59:12 -0500

    feat(coords): add more way that do the things

---
+++ constants.py
@@ -0,0 +1,25 @@
+"""
+- constants
+"""
+# GLOBALS VARS
+brakets = "-" * 30
+
+### coords [x, y]
+ICON_HOME = ["1536", "50"]
+ICON_BACK = ["50", "50"]
+ICON_SEND_TASK = ["1400", "800"]
+BUTTON_CLAIN_ALL = ["1550", "180"]
+
+# tip => do it steps manual one time and write steps for implement with coords [[x,y], [x,y], [x,y]]
+COORDS_WITHOUT = [["86", "240"], ["850", "850"], ["850", "850"], ["69", "275"],
+                  ["1350", "800"], ["1350", "800"], ICON_HOME, ["1090", "360"],
+                  ["1470", "160"], ["1470", "160"], ICON_HOME, ["1200", "330"],
+                  ["1200", "840"], ["1400", "840"], ICON_HOME, ["1530", "830"],
+                  ["270", "580"], ["1200", "680"], ["1200", "680"],
+                  ["1300", "800"], ["1080", "612"], ["1080", "612"], ICON_BACK,
+                  ["1520", "360"], ["530", "840"], ["530", "840"],
+                  ["140", "270"], ICON_SEND_TASK,
+                  ["60", "150"], ICON_SEND_TASK, ["60", "280"], ICON_SEND_TASK,
+                  ["60", "440"], ICON_SEND_TASK, ["60", "590"], ICON_SEND_TASK,
+                  ["60", "760"], ICON_SEND_TASK, ICON_HOME]
+# look part when ICON_BACK & ICON_HOME are used so close
--- main.py
+++ main.py
@@ -3,70 +3,59 @@ Cerated on Fri Nov 19 10:51 AM - 2021
 
 @author: Juanestban
 """
-import requests, sys, signal, time
+import sys, signal, time
 from ppadb.client import Client
+from constants import COORDS_WITHOUT, brakets
 
 
-def handler_Exit(sig, frame):
-    print('')
+def handle_exit():
+    print("")
     print('-' * 80)
     print('\n[*] Exiting...\n')
     sys.exit(0)
 
 
-signal.signal(signal.SIGINT, handler_Exit)
+signal.signal(signal.SIGINT, handle_exit)
 
-
-def runCommad(cmd):
-    if (cmd == 'exit'):
-        handler_Exit()
-
-
-### coords [x, y]
-iconHome = ["1536", "50"]
-iconBack = ["50", "50"]
-iconSendTask = ["1400", "800"]
-ButtonClainAll = ["1550", "180"]
-# tip => do it steps manual one time and write steps for implement with coords [x,y]
-coordsNew = [
-    "86", "240", "850", "850", "850", "850", *iconHome, "1090", "360", "1470",
-    "160", "1470", "160", *iconHome, "1200", "330", "1200", "840", "1400",
-    "840", *iconHome, "1520", "830", "270", "580", "1200", "680", "1200",
-    "680", "1300", "800", "1080", "612", *iconBack, "1520", "360", *iconHome,
-    "140", "270", *iconSendTask, "60", "150", *iconSendTask, "60", "280",
-    *iconSendTask, "60", "440", *iconSendTask, "60", "590", *iconSendTask,
-    "60", "760", *iconSendTask, *iconHome
-]
-coordsWithout = [
-    "86", "240", "850", "850", "850", "850", *iconHome, "1090", "360", "1470",
-    "160", "1470", "160", *iconHome, "1200", "330", "1200", "840", "1400",
-    "840", *iconHome, "1530", "830", "270", "580", "1200", "680", "1200",
-    "680", "1300", "800", "1080", "612", *iconBack, "1520", "360", "140",
-    "270", *iconSendTask, "60", "150", *iconSendTask, "60", "280",
-    *iconSendTask, "60", "440", *iconSendTask, "60", "590", *iconSendTask,
-    "60", "760", *iconSendTask, *iconHome
-]
-# look part when iconBack & iconHome are used so close
 
 ### Client ADB Device
-client = Client(host="127.0.0.1", port=5037)
-devices = client.devices()
-device = devices[0]
-print(len(coordsNew) / 2, len(coordsWithout) / 2)
+def getClient():
+    try:
+        client = Client(host="127.0.0.1", port=5037)
+        devices = client.devices()
+        return devices[0]
+    except:
+        print(brakets, "[+] Error to try to get your device", brakets)
+        inputAnswer = int(
+            input(
+                "\nDo you want retry find some devices? [YES=1] [NO=0]:\n$/:"))
+
+        if inputAnswer == 0:
+            handle_exit()
+            return
+
+        getClient()
+
+
+device = getClient()
+
 ### Exce command
 # device.shell("input touchscreen tap {x} {y}".format(x=coords[4], y=coords[5]))
 
-count = 0
-step = 1
-stepsComplete = len(coordsWithout) / 2
+step = 0
+stepsComplete = len(COORDS_WITHOUT) - 1
 
 while stepsComplete >= step:
-    count += 2
-    coords = coordsWithout
-    x = coords[count - 2]
-    y = coords[count - 1]
+    coords = COORDS_WITHOUT
+    x = coords[step][0]
+    y = coords[step][1]
 
     device.shell("input touchscreen tap {x} {y}".format(x=x, y=y))
-    print(x, y)
-    input('are you wish continue?\n')
+    print('coordX:', x, "coordY:", y, "DATA:", coords[step])
+    answer = input('Are you wish continue? [y:yes] [n:no]\n')
+
+    if answer == "n":
+        handle_exit()
+
+    # time.sleep(2)
     step += 1
